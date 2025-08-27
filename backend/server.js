343c0394c770cd7781a3eed8eeb96471b98e155c
Joseph Ruano <josphrgz@aol.com>
2025-08-26 23:10:58 -0600

    actualizacion

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import morgan from 'morgan'; 
+import morgan from 'morgan'
 
 const app = express();
 const PORT = process.env.PORT || 9000;
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
     res.status(200).json({ message: 'status:ok' });
 });
 
-app.get('/prueba', (req, res) => {
+app.get('/api/prueba', (req, res) => {
     res.status(200).json({ message: 'Este es solo de prueba, pronto se tendrá más si Dios quiere y lo permite' });
 });
 
