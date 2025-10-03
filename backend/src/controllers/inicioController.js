f93bc1e6bb4229e483b1ffa5a3d75ac3b6b4f9a0
Joseph Ruano <git@josephinfra.dev>
2025-10-03 10:14:43 -0600

    feat: ampliar mensaje de bienvenida en inicioController

--- backend/src/controllers/inicioController.js
+++ backend/src/controllers/inicioController.js
@@ -5,9 +5,11 @@ exports.obtenerInicio = async (req, res) => {
   if (!usuario) return res.status(401).json({ mensaje: 'No autenticado' });
   try {
     const resultados = await pool.query(
-      `SELECT p.nombre, p.apellido_paterno, p.apellido_materno
+      `SELECT u.idUsuario, u.usuario, p.nombre, p.apellido_paterno, p.apellido_materno, lp.nombre AS perfil
        FROM dbo_usuario u
        INNER JOIN dbo_persona p ON u.idPersona = p.idPersona
+       LEFT JOIN dbo_usuario_perfil up ON u.idUsuario = up.idUsuario
+       LEFT JOIN dbo_login_perfil lp ON up.idPerfil = lp.idPerfil
        WHERE u.idUsuario = ?`,
       [usuario.idUsuario]
     );
@@ -16,13 +18,10 @@ exports.obtenerInicio = async (req, res) => {
     }
     const persona = resultados[0];
     const nombreCompleto = `${persona.nombre} ${persona.apellido_paterno} ${persona.apellido_materno}`;
-
-    const datosPersonales = {
-      nombreCompleto: nombreCompleto,
-    };
+    const nombrePerfil = `${persona.perfil}`;
 
     res.json({
-      mensaje: `¡Bienvenido, ${nombreCompleto}!`
+      mensaje: `¡Bienvenido ${nombrePerfil}, ${nombreCompleto}!`
     });
   } catch (error) {
     console.error('Error en inicio:', error);
