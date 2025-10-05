6078b6c19f9e70c3dc80d0da576c796cc27ad607
Joseph Ruano <josphrgz@aol.com>
2025-10-04 20:01:52 -0600

    Validacion de longitud mínima y máxima del usuario y contraseña

--- backend/src/validators/authValidator.js
+++ backend/src/validators/authValidator.js
@@ -1,14 +1,14 @@
 const Joi = require('joi');
 
 const loginSchema = Joi.object({
-  usuario: Joi.number().integer().min(10000000).required().messages({
+  usuario: Joi.number().integer().min(10000000).max(10000000).required().messages({
     'number.base': 'El usuario debe ser un número',
-    'number.min': 'El usuario debe tener al menos 8 dígitos',
+    'number.min': 'El usuario debe tener 8 dígitos',
     'any.required': 'El campo usuario es obligatorio'
   }),
-  contrasena: Joi.number().integer().min(1000).required().messages({
+  contrasena: Joi.number().integer().min(1000).max(1000).required().messages({
     'number.base': 'La contraseña debe ser un número',
-    'number.min': 'La contraseña debe tener al menos 4 dígitos',
+    'number.min': 'La contraseña debe tener 4 dígitos',
     'any.required': 'El campo contraseña es obligatorio'
   })
 });
