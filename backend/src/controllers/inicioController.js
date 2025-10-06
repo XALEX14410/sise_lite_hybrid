bae8beaf1c6d52a874e9751eb3a4a0e826470ef2
Joseph Ruano <josphrgz@aol.com>
2025-10-05 19:35:27 -0600

    feat: creación de endpoint inicio para docentes/alumnos/admin

--- backend/src/controllers/inicioController.js
+++ backend/src/controllers/inicioController.js
@@ -3,9 +3,10 @@ const pool = require('../db/pool');
 exports.obtenerInicio = async (req, res) => {
   const usuario = req.session.usuario;
   if (!usuario) return res.status(401).json({ mensaje: 'No autenticado' });
+
   try {
     const resultados = await pool.query(
-      `SELECT u.idUsuario, u.usuario, p.nombre, p.apellido_paterno, p.apellido_materno, lp.nombre AS perfil
+      `SELECT u.idUsuario, u.usuario, p.nombre, p.apellido_paterno, p.apellido_materno, lp.nombre AS perfil, lp.idPerfil
        FROM dbo_usuario u
        INNER JOIN dbo_persona p ON u.idPersona = p.idPersona
        LEFT JOIN dbo_usuario_perfil up ON u.idUsuario = up.idUsuario
@@ -13,18 +14,51 @@ exports.obtenerInicio = async (req, res) => {
        WHERE u.idUsuario = ?`,
       [usuario.idUsuario]
     );
+
     if (resultados.length === 0) {
       return res.status(404).json({ mensaje: 'No se encontró el nombre del usuario' });
     }
+
     const persona = resultados[0];
     const nombreCompleto = `${persona.nombre} ${persona.apellido_paterno} ${persona.apellido_materno}`;
-    const nombrePerfil = `${persona.perfil}`;
+    const nombrePerfil = persona.perfil;
+    const idPerfil = persona.idPerfil;
 
-    res.json({
-      mensaje: `¡Bienvenido ${nombrePerfil}, ${nombreCompleto}!`
+    let datosGenerales = null;
+
+    if (idPerfil === 1 || idPerfil === 2) {
+      // Si es Admin o Superadmin
+      const [docentes, estudiantes, materias, carreras, roles, administradores] = await Promise.all([
+        pool.query(`SELECT COUNT(*) AS cantidad FROM dbo_docente`),
+        pool.query(`SELECT COUNT(*) AS cantidad FROM dbo_alumno`),
+        pool.query(`SELECT COUNT(*) AS cantidad FROM dbo_materias`),
+        pool.query(`SELECT COUNT(*) AS cantidad FROM dbo_carrera`),
+        pool.query(`SELECT COUNT(*) AS cantidad FROM dbo_login_perfil`),
+        pool.query(`SELECT COUNT(*) AS cantidad FROM dbo_usuario_perfil WHERE idPerfil = 1 OR idPerfil = 2`)
+      ]);
+
+      datosGenerales = {
+        docentesRegistrados: Number(docentes[0].cantidad),
+        estudiantesRegistrados: Number(estudiantes[0].cantidad),
+        materiasRegistradas: Number(materias[0].cantidad),
+        carrerasRegistradas: Number(carreras[0].cantidad),
+        rolesRegistrados: Number(roles[0].cantidad),
+        administradoresRegistrados: Number(administradores[0].cantidad)
+      };
+
+      return res.json({
+      mensaje: `¡Bienvenido ${nombrePerfil}, ${nombreCompleto}!`,
+      datosGenerales
     });
+    }
+
+    return res.json({
+      mensaje: `¡Bienvenido ${nombrePerfil}, ${nombreCompleto}!`,
+    });
+
   } catch (error) {
     console.error('Error en inicio:', error);
-    res.status(500).json({ mensaje: 'Error interno del servidor' });
+    res.status(500).json({ mensaje: 'Error interno del servidor', detalle: JSON.stringify(error.message)});
   }
 };
+
