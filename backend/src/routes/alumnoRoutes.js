c7a5c797ec9b9e32ff59105cec19f375ff5c5b0e
Joseph Ruano <git@josephinfra.dev>
2025-10-14 16:30:05 -0600

    feat: modelos y controladores definidos

--- backend/src/routes/alumnoRoutes.js
+++ backend/src/routes/alumnoRoutes.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const router = express.Router();
-
+const validar = require('../middlewares/validar');
+const idParamSchema = require('../validators/idValidator');
 const verificarSesion = require('../middlewares/verificarSesion');
-const verificarPerfil = require('../middlewares/verificarPerfil');
-
+//const verificarPerfil = require('../middlewares/verificarPerfil');
 const alumnoController = require('../controllers/alumnoController');
-const calificacionesController = require('../controllers/calificacionesController');
-const pagosController = require('../controllers/pagosController');
-const becasController = require('../controllers/becasController');
-const horarioController = require('../controllers/horarioController');
-const inscripcionesController = require('../controllers/inscripcionesController');
 
-
+router.get('/:id/calificaciones', validar(idParamSchema, 'params'), verificarSesion, alumnoController.obtenerCalificacionesPorAlumno);
 
 module.exports = router;
